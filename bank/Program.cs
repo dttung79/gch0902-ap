5bfb0a3bc899709545ba56cbd06dc1fe483afba8
dttung79 <dttung@gmail.com>
2021-11-18 16:55:38 +0700

    Finished Bank Project

--- bank/Program.cs
+++ bank/Program.cs
@@ -4,16 +4,52 @@ namespace bank
 {
     class Program
     {
+        private Bank vcb;
+        public Program()
+        {
+            vcb = new Bank();
+        }
         static void Main(string[] args)
         {
-            Account acc01 = new Account(); // call default constructor
-            Account acc02 = new Account("Nguyen Van Linh", 1000);
+            Program p = new Program();
+            p.Run();
+        }
 
-            acc01.ShowInfo();
-            acc02.ShowInfo();
+        public void Run()
+        {
+            while (true)
+            {
+                PrintMenu();
+                int choice = GetChoice();
+                DoTask(choice);
+                if (choice == 0) break;
+            }
+        }
 
-            Account acc03 = new Account("John Lennon", 5000);
-            acc03.ShowInfo();
+        private void PrintMenu()
+        {
+            System.Console.WriteLine("Bank Menu");
+            System.Console.WriteLine("1. Add account.");
+            System.Console.WriteLine("2. Show account.");
+            System.Console.WriteLine("0. Exit.");
+        }
+        private int GetChoice()
+        {
+            System.Console.Write("Enter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            return choice;
+        }
+
+        private void DoTask(int choice)
+        {
+            switch(choice)
+            {
+                case 1: vcb.AddAccount(); break;
+                case 2: vcb.ShowAccount(); break;
+                case 0: break;
+                default: System.Console.WriteLine("Invalid Option!"); break;
+            }
         }
     }
 }
+    
