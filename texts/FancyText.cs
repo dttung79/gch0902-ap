29c20026fac68c4e33172672a562bc430e10f006
dttung79 <dttung@gmail.com>
2021-11-23 15:15:14 +0700

    create Text project

---
+++ texts/FancyText.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace texts
+{
+    public class FancyText
+    {
+        // TODO:
+        // FancyText inherites from Text
+        // Implement: default constructor, parameter constructor
+        // Override Print method to print content in fancy way
+    }
+}
